485a8137b6551af77a1d51dcd500d12caeb29410
Ram Santhosh Patnaik Belagam <97543273+Ram5anthosh@users.noreply.github.com>
2022-07-26 11:12:55 +0530

    Adding draw function

---
+++ draw.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+void draw(bool o[9], bool x[9]){
+  cout<<"\033[2J\033[1;1H";
+
+  for(int i=0;i<9;i++){
+    if(o[i]==0 && x[i]==0)
+      cout<<i+1<<" ";
+    if(o[i]==1)
+      cout<<"O"<<" ";
+    if(x[i]==1)
+      cout<<"X"<<" ";
+
+    if((i+1)%3==0)
+      cout<<endl;
+  }
+}
