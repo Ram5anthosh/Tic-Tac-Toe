0955f204358883cba2e17d6d4cc6f1cc5076fb64
Ram Santhosh Patnaik Belagam <97543273+Ram5anthosh@users.noreply.github.com>
2022-07-26 11:13:58 +0530

    Adding check function

---
+++ check.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+
+using namespace std;
+
+bool check(bool o[9],bool x[9],int d){
+  int z=0;
+  bool b=0;
+  for(int i=0;i<9; i += 3 ){
+    if(o[i] && o[i+1] && o[i+2]){
+      b=1;
+      z=1;
+    }
+    if(x[i] && x[i+1] && x[i+2]){
+      b=1;
+      z=2;
+    }
+  }
+
+  for(int i=0;i<3;i++){
+    if(o[i] && o[i+3] && o[i+6]){
+      b=1;
+      z=1;
+    }
+    if(x[i] && x[i+3] && x[i+6]){
+      b=1;
+      z=2;
+    }
+  }
+
+  for(int i=0;i<2;i++)
+  {
+    int k=4-2*i;
+    int j=2*i;
+    if(o[j] && o[j+k] && o[j+2*k] ){
+      b=1;
+      z=1;
+    }
+    if(x[j] && x[j+k] && x[j+2*k]){
+      b=1;
+      z=2;
+    }
+  }
+
+  if(b==1){
+    if(z==1){
+      cout<<"Player 1 wins\n";
+    }
+    else if(z==2){
+      cout<<"Player 2 wins\n";
+    }
+    return true;
+  }
+
+  if(b==0 && d==9){
+    cout<<"It's a draw\n";
+    return true;
+  }
+
+  return false;
+
+}
