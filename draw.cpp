2bbf5ef3941661ba4d7d5451b2a3c81af982fcf2
Ram5anthosh <ramsanthosh2112@gmail.com>
2022-09-12 23:58:13 +0530

    Added feature to display player name

--- draw.cpp
+++ draw.cpp
@@ -1,20 +1,22 @@
-#include<iostream>
+#include <iostream>
 
 using std::cout;
 using std::endl;
 
-void draw(bool o[9], bool x[9]){
-  cout<<"\033[2J\033[1;1H";
+void draw(bool o[9], bool x[9]) {
+  cout << "\033[2J\033[1;1H";
 
-  for(int i=0;i<9;i++){
-    if(o[i]==0 && x[i]==0)
-      cout<<i+1<<" ";
-    if(o[i]==1)
-      cout<<"O"<<" ";
-    if(x[i]==1)
-      cout<<"X"<<" ";
+  for (int i = 0; i < 9; i++) {
+    if (o[i] == 0 && x[i] == 0)
+      cout << i + 1 << " ";
+    if (o[i] == 1)
+      cout << "O"
+           << " ";
+    if (x[i] == 1)
+      cout << "X"
+           << " ";
 
-    if((i+1)%3==0)
-      cout<<endl;
+    if ((i + 1) % 3 == 0)
+      cout << endl;
   }
 }
--- main.cpp
+++ main.cpp
@@ -3,52 +3,56 @@
 using namespace std;
 
 void draw(bool o[9], bool x[9]);
-bool check(bool o[9],bool x[9],int i);
+bool check(bool o[9], bool x[9], int i, string *first, string *second);
 int input(int a);
+void name(string *a, string *b);
 
-int main (int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int a;
 
+  string player1;
+  string player2;
 
-  bool o[9],x[9];
+  name(&player1, &player2);
 
-  for(int i=0;i<9;i++){
-    o[i]=0;
-    x[i]=0;
+  bool o[9], x[9];
+
+  for (int i = 0; i < 9; i++) {
+    o[i] = 0;
+    x[i] = 0;
   }
 
-  int d=0;
+  int d = 0;
 
-  for(int i=0;i<5 && check(o,x,i)==false;i++){
+  for (int i = 0; i < 5 && check(o, x, i, &player1, &player2) == false; i++) {
 
-    draw(o,x);
+    draw(o, x);
 
-    cout<<"Player 1 enter the block of your choice : ";
-    cin>>a;
-    //d++;
-    a=input(a-1);
-    o[a]=1;
+    cout << player1 << " enter the block of your choice : ";
+    cin >> a;
+    // d++;
+    a = input(a - 1);
+    o[a] = 1;
     d++;
 
-    draw(o,x);
+    draw(o, x);
 
-    if(check(o,x,d)){
-       return 0;
+    if (check(o, x, d, &player1, &player2)) {
+      return 0;
     }
 
-    cout<<"Player 2 enter the block of your choice : ";
-    cin>>a;
-    a=input(a-1);
-    x[a]=1;
+    cout << player2 << " enter the block of your choice : ";
+    cin >> a;
+    a = input(a - 1);
+    x[a] = 1;
     d++;
 
-    draw(o,x);
+    draw(o, x);
 
-    if(check(o,x,d)){
+    if (check(o, x, d, &player1, &player2)) {
       return 0;
     }
   }
-  
+
   return 0;
 }
